a9beba205520f76a6a8777e1d7d06aca42ab310a
Reid Poole <poole.reid@gmail.com>
2024-03-27 22:49:14 -0600

    Only Show events that have been approved throug adminService

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -2,7 +2,7 @@
 import { getEvents } from "@/pages/api/route";
 import { useState, useEffect } from "react";
 import "../styles/globals.css";
-import EventCard from "./eventCard";
+import EventCard from "./EventCard";
 const EventsPage: React.FC = () => {
   const [events, setEvents] = useState([]);
 
@@ -10,7 +10,8 @@ const EventsPage: React.FC = () => {
     const fetchData = async () => {
       try {
         const eventsData = await getEvents();
-        setEvents(eventsData);
+        const approvedEvents = eventsData.filter((activity:any) => activity.is_approved)
+        setEvents(approvedEvents);
       } catch (error) {
         console.error('Failed to load events', error);
       }
@@ -22,13 +23,17 @@ const EventsPage: React.FC = () => {
  return (
     <div>
       <div className="max-h-screen overflow-y-auto">
-        <ul className="space-y-4">
-          {events.map((event: any) => (
-            <li key={event.id} className="border p-4 rounded shadow">
-              <EventCard event={event}/>
-            </li>
-          ))}
-        </ul>
+        {events && events.length > 0 ? (
+          <ul className="space-y-4">
+            {events.map((event: any) => (
+              <li key={event.id} className="border p-4 rounded shadow">
+                <EventCard event={event}/>
+              </li>
+              ))}
+          </ul>
+          ) : (
+            <p>No Events!!</p>
+          )}
       </div>
     </div>
   );
