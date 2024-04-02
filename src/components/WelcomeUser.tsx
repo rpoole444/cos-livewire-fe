3584bd80907fd9a66a82b08abc2bc7e991679910
Reid Poole <poole.reid@gmail.com>
2024-04-01 19:43:36 -0600

    add conditional if admin then give access to other pages using these buttons

--- src/components/WelcomeUser.tsx
+++ src/components/WelcomeUser.tsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 import { logoutUser } from "@/pages/api/route";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/router";
+import { User } from "@/interfaces/interfaces";
 
 
 const WelcomeUser = () =>{
-  const { user, logout} = useAuth()
+  const { user, logout} = useAuth() as { user: User | null, logout: () => void };
   const router = useRouter()
   
   const handleLogout = async () => {
@@ -21,18 +22,29 @@ const WelcomeUser = () =>{
   return (
     <>
       <h1 className="text-center text-sm md:text-base lg:text-lg p-2 bg-blue-100 text-blue-900 font-semibold rounded-md shadow">
-          Welcome! You are logged in.
+        Welcome, {user?.first_name}! You are logged in.
       </h1>
       <br/>
-      <Link href="/eventSubmission" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-        Submit Event
-      </Link>
-      <button  onClick={handleLogout} className="mt-4 text-blue-500">
-        Logout
-      </button>
+      <div className="flex flex-col items-center">
+        <Link href="/eventSubmission" className="mb-4 px-4 py-2 bg-indigo-600 text-white font-medium rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 text-sm text-center">
+            Submit Event
+        </Link>
+        {user?.is_admin && (
+          <>
+            <Link href="/AdminUsersPage" className="mb-4 px-4 py-2 bg-green-600 text-white font-medium rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 text-sm text-center">
+                Admin Settings
+            </Link>
+            <Link href="/adminService" className="mb-4 px-4 py-2 bg-blue-600 text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 text-sm text-center">
+                Review Events
+            </Link>
+          </>
+        )}
+        <button onClick={handleLogout} className="text-blue-500">
+          Logout
+        </button>
+      </div>
     </>
-  )
-
-}
+  );
+};
 
 export default WelcomeUser;
