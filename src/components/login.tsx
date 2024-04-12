8a824c0b7f583d0721ad433c0846c7b0b9bdd56a
Reid Poole <poole.reid@gmail.com>
2024-04-12 11:52:42 -0600

    added a way to reset your password

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -87,6 +87,9 @@ const LoginForm: React.FC = () => {
               <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 Sign in
               </button>
+              <Link href="/forgot-password" className="mt-8 text-blue-500 ">
+                  Forget your password? Click Here!
+              </Link>
             </div>
           </form>
         </div>
