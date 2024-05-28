34c567b47279f1f6454cde12c03c50a3df61d2c4
Reid Poole <poole.reid@gmail.com>
2024-05-27 23:15:27 -0600

    Add an Image for Header!

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import Image from 'next/image';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -11,9 +12,12 @@ const Header = () => {
 
   return (
     <header className="w-full bg-indigo-800 text-white p-6 shadow-md flex justify-between items-center">
-      <h1 className="text-4xl lg:text-5xl font-bold tracking-tight">
-        Alpine Groove Guide
-      </h1>
+      <div className="flex items-center">
+        <Image src="/alpine_groove_guide_icon.png" alt="Alpine Groove Guide Icon" width={70} height={70} />
+        <h1 className="text-4xl lg:text-5xl font-bold tracking-tight ml-4">
+          Alpine Groove Guide
+        </h1>
+      </div>
       <div className="relative">
         <button onClick={toggleMenu} className="text-white focus:outline-none lg:hidden">
           {menuOpen ? <AiOutlineClose size={28} /> : <AiOutlineMenu size={28} />}
