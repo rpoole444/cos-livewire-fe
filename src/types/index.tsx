b7a4f092b531f4f29268b50b779665df796518e2
Reid Poole <poole.reid@gmail.com>
2024-06-01 22:56:23 -0600

    Add Type property value

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -4,4 +4,6 @@ export interface UserType {
   first_name: string; // or string, depending on your
   last_name: string; // or string, depending on your
   email: string;
+  user_description: string; // or string, depending on your
+  top_music_genres: string; // or string, depending on your
 }
