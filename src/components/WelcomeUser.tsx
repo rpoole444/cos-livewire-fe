2bed3c19dfcdac7337722ee46fd8738001f88970
Reid Poole <poole.reid@gmail.com>
2024-06-01 22:57:51 -0600

    Add User Profile Link

--- src/components/WelcomeUser.tsx
+++ src/components/WelcomeUser.tsx
@@ -1,44 +1,45 @@
+// components/WelcomeUser.tsx
 import Link from "next/link";
-import { logoutUser } from "@/pages/api/route";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/router";
 import { User } from "@/interfaces/interfaces";
 
+const WelcomeUser = () => {
+  const { user, logout } = useAuth() as { user: User | null, logout: () => void };
+  const router = useRouter();
 
-const WelcomeUser = () =>{
-  const { user, logout} = useAuth() as { user: User | null, logout: () => void };
-  const router = useRouter()
-  
   const handleLogout = async () => {
-    try {  
-      // await logoutUser()
-        logout()
-        router.push('/')
+    try {
+      logout();
+      router.push("/");
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
   };
-  console.log(user)
+
   return (
     <>
       <h1 className="text-center text-sm md:text-base lg:text-lg p-2 bg-blue-100 text-blue-900 font-semibold rounded-md shadow">
         Welcome, {user?.first_name}! You are logged in.
       </h1>
-      <br/>
+      <br />
       <div className="flex flex-col items-center">
         <Link href="/eventSubmission" className="mb-4 px-4 py-2 bg-indigo-600 text-white font-medium rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 text-sm text-center">
-            Submit Event
+          Submit Event
         </Link>
         {user?.is_admin && (
           <>
             <Link href="/AdminUsersPage" className="mb-4 px-4 py-2 bg-green-600 text-white font-medium rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 text-sm text-center">
-                Admin Settings
+              Admin Settings
             </Link>
             <Link href="/adminService" className="mb-4 px-4 py-2 bg-blue-600 text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 text-sm text-center">
-                Review Events
+              Review Events
             </Link>
           </>
         )}
+        <Link href="/UserProfile" className="mb-4 px-4 py-2 bg-yellow-500 text-white font-medium rounded-md hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 text-sm text-center">
+          User Profile
+        </Link>
         <button onClick={handleLogout} className="text-blue-500">
           Logout
         </button>
