aa678cf98510cd2d7a825c6c01283f701d98879e
Reid Poole <poole.reid@gmail.com>
2024-06-30 10:53:12 -0600

    Config the images from the S3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  images: {
+    domains: ['alpinegg-posters.s3.us-east-2.amazonaws.com'],
+  },
+}
 
 export default nextConfig;
