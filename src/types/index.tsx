64a4160fdc6fad0f8a24c9dcb3e7bbdac68553d7
Reid Poole <poole.reid@gmail.com>
2024-11-17 12:42:51 -0700

    Add Usertype id to numberconstructor

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,6 +1,6 @@
 
 export interface UserType {
-  id: string; // or number, depending on your ID system
+  id: NumberConstructor; // or number, depending on your ID system
   first_name: string; // or string, depending on your
   last_name: string; // or string, depending on your
   email: string;
