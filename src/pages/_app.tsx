906c5530eaf8c717b91c0b559bdcc2c5339772c7
Reid Poole <poole.reid@gmail.com>
2025-05-10 21:28:23 -0600

    fix _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,4 @@
-// src/pages/_app.tsx
 import React from 'react';
-import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { AuthProvider } from '../context/AuthContext';
 import '../styles/globals.css';
@@ -8,12 +6,6 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Component {...pageProps} />
     </AuthProvider>
   );
