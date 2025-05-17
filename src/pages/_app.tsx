9f89fbe972280ce57fe0aa54794417f414ad11e9
Reid Poole <poole.reid@gmail.com>
2025-05-17 09:13:52 -0600

    error handling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,17 @@ import { AppProps } from 'next/app';
 import { AuthProvider } from '../context/AuthContext';
 import '../styles/globals.css';
 
+if (typeof window !== 'undefined') {
+  window.addEventListener('error', e => {
+    /* e.error is the original Error object */
+    console.log('🔥 Global error:', e.error?.message);
+    console.log(e.error?.stack);
+  });
+  window.addEventListener('unhandledrejection', e => {
+    console.log('🔥 Promise rejection:', e.reason);
+  });
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
